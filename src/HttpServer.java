44217dcd70eb046b3f35109b778db7d4f32cb81e
joaopedroazul <joaopedro.dev.sda@gmail.com>
2025-08-31 19:51:04 -0300

    Feature: criando a classe HttpServer

---
+++ src/HttpServer.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HttpServer {
+    private final int port;
+    private ServerSocket serverSocket;
+    private ExecutorService executor;
+    private boolean isRunning;
+
+    public HttpServer(int port) {
+        this.port = port;
+        this.executor = Executors.newFixedThreadPool(10);
+    }
+    public void start() throws IOException {
+        serverSocket = new ServerSocket(port);
+        isRunning = true;
+
+        System.out.println("Servidor iniciado na Porta " + port);
+    }
+
+    public void stop() throws IOException {
+        isRunning = false;
+        executor.shutdown();
+        serverSocket.close();
+        System.out.println("Servidor encerrado na Porta " + port);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -5,10 +6,12 @@ import java.time.LocalDate;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args)  throws SQLException {
-        ConnectDB.initDB();
-        ConnectDB.testConnection();
+    public static void main(String[] args) throws SQLException, IOException {
 
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "5000"));
+        HttpServer server = new HttpServer(port);
+        server.start();
+        server.stop();
 
     }
 }
